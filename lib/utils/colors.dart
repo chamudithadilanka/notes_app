90bf4d1cf0b8ff40f30a9d3094841dc075663843
uniqudev <158754682+uniqudev@users.noreply.github.com>
2025-08-28 11:27:51 +0530

    create app and setup app

---
+++ lib/utils/colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+const kCardBgColor = Color(0xffff5f5f5);
+const ksubtitleColor = Colors.grey;
+const kMainWhiteColor = Colors.white;
+const kMainColor = Color(0xfff0099ff);
+const kMainNavSelected = Color(0xfff0193FC);
+
+const kMainBlackColor = Color(0xfff333333);
+const kMainDarkBlue = Color(0xfff0147fc);
+const kMainPinkColor = Color(0xfffff0000);
+
+const kMianLightBlueColor = Color(0xfff00f0ff);
+const kMainLightPurpleColor = Color(0xfffeb00ff);
+const kSubPinkColor = Color(0xfffff006b);
